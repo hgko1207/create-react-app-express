019c248bcaf7db4e26edcfb4c9d1952e47e09e1d
hgko1207 <khkkhk1207@gmail.com>
2020-03-05 12:00:12 +0900

    server setting

--- server.js
+++ server.js
@@ -16,4 +16,14 @@ app.post("/api/world", (req, res) => {
   res.send(`I received your POST request. This is what you sent me: ${req.body.post}`);
 });
 
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
